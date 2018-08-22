08ae008d49dd799ebcd5d90dc43637577d47b8f4
Caíque <40068062+apieceofCAKE@users.noreply.github.com>
2018-08-22 19:27:16 -0300

    Update main.c
    Added comments.

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+//This program receive words and numbers to check if they are palindromes
+
 int main() {
 
+    //Max size for the input is 49 characters
     char palavra [50];
     int tamanho = 0;
+    //Auxiliary variables
     int aux1 = 0;
     int aux2 = 0;
 
@@ -13,6 +17,7 @@ int main() {
     scanf("%s", palavra);
     printf("   \n");
 
+    //Loop ends when it reaches NUL character
     while (palavra[tamanho] != '\0') {
 
         tamanho ++;
@@ -26,6 +31,7 @@ int main() {
         aux1 ++;
     }
 
+    //aux2 acts as a counter for characters that are the same as its correspondent backwards-wise
     if (aux2 == tamanho)
         printf("A palavra %s é palindromo.\n", palavra);
     else
